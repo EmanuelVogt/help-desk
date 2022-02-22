d5b9e4657e6954d53affc0cdc7a4e6072a917c1c
EmanuelVogt <contatoemanuel3@gmail.com>
2022-02-22 17:56:58 -0300

    create security views

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Home } from '@screens/Home';
 import { AuthRoutes } from './auth.routes';
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 export function Routes() {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(setUser);
+
+    return subscriber;
+  }, []);
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user ? <Home /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
